4547a1ea92b84cee69e3b51e933cdb2deb9e824f
AbdulrahmanHaniAUC <AbdulrahmanHani@aucegypt.edu>
2023-06-15 22:43:01 +0300

    updated mqueens.h

--- mqueens.h
+++ mqueens.h
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-bool isSafe(vector<vector<int>>& board, int row, int col, int N);
-bool solveNQueens(vector<vector<int>>& board, int row, int N);
-void printBoard(const vector<vector<int>>& board);
+bool isSafe(vector<vector<int> >& board, int row, int col, int N);
+bool solveNQueens(vector<vector<int> >& board, int row, int N);
+void printBoard(const vector<vector<int> >& board);
 void solveNQueens(int N);
